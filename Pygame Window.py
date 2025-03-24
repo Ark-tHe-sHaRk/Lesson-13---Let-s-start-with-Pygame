23f823aef3436abaab696748c7f85ff4b376b31b
Ark-tHe-sHaRk <arkbarot8812coder@gmail.com>
2025-03-24 16:51:48 +0000

    Add initial Pygame window setup and event loop

---
+++ Pygame Window.py
@@ -0,0 +1,22 @@
+#Importing Nessasary Libraries
+import pygame
+
+#Initialising required modules
+pygame.init()
+
+#Set up window geometry
+screen = pygame.display.set_mode((600, 500))
+
+#Create a loop to keep the game running until the user closes the window
+done = False
+
+while not done:
+    
+    #Keep clearing the even queue
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    #Make the changes visible
+    pygame.display.flip()
+
